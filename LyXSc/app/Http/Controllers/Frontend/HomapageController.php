cefc8d7312a27f3f3e989a4304683664cbad4ac2
Leave-it-blank <leaveitblank32@gmail.com>
2022-07-12 22:12:36 +0000

    unfinished comics page

--- LyXSc/app/Http/Controllers/Frontend/HomapageController.php
+++ LyXSc/app/Http/Controllers/Frontend/HomapageController.php
@@ -34,7 +34,7 @@ class HomapageController extends Controller
         //     ];
         // })->toArray());
         return Inertia::render('Frontend/HomePage', [
-            'comics' => Comic::where('isHidden', false)->orderBy('updated_at', 'asc')->with('volumes.chapters')->take(4)->get()->map(function ($comic) {
+            'comics' => Comic::where('isHidden', false)->orderBy('updated_at', 'asc')->with(['volumes', 'volumes.chapters'])->take(4)->get()->map(function ($comic) {
                 return [
                     'id' => $comic->id,
                     'title' => $comic->title,
@@ -54,7 +54,7 @@ class HomapageController extends Controller
 
                 ];
             })->toArray(),
-            'recommended' => Comic::where('isHidden', false)->orderByViews('asc')->with('volumes.chapters')->take(4)->get()->map(function ($comic) {
+            'recommended' => Comic::where('isHidden', false)->orderByViews('asc')->with(['volumes', 'volumes.chapters'])->take(4)->get()->map(function ($comic) {
                 return [
                     'id' => $comic->id,
                     'title' => $comic->title,
@@ -93,6 +93,29 @@ class HomapageController extends Controller
 
     public function viewComic(Comic $comic)
     {
-       dd($comic);
+        $comic = Comic::where('id',$comic->id)->with(['volumes', 'volumes.chapters'])->get();
+          //dd($comic);
+        return Inertia::render('Frontend/Comics/ViewComic', [
+            'comic' => [
+                    'id' => $comic[0]->id,
+                    'title' => $comic[0]->title,
+                    'viewUrl' => 'reader.comic.view',
+                    'titleslug' => $comic[0]->titleSlug,
+                    'isMature' => $comic[0]->isMature,
+                    'isLocked' => $comic[0]->isLocked,
+                    'createdAt' => $comic[0]->created_at,
+                    'updatedAt' => $comic[0]->updated_at,
+                    'thumb' => $comic[0]->getFullThumbnailUrl(),
+                    'volumes'=> $comic[0]->volumes->map(function ($volume) {
+                        return [
+                               'chapters' => $volume->chapters->reverse(),
+                               'name' => $volume->name,
+                               'number' => $volume->number,
+                               'id' => $volume->id
+
+                        ];}),
+
+                ]
+        ]);
     }
 }
---
+++ LyXSc/resources/js/Pages/Frontend/Comics/ViewComic.vue
@@ -0,0 +1,32 @@
+<template>
+<ReaderLayout>
+  <div class="flex flex-row justify-center">
+
+    <div  >
+      <img class="w-60 h-72" :src="props.comic.thumb">
+</div>
+  </div>
+
+
+</ReaderLayout>
+
+</template>
+
+<script setup>
+
+import { Head } from '@inertiajs/inertia-vue3'
+import { computed, ref, onMounted } from 'vue'
+import { useComicStore } from '@/stores/comic'
+import ComicCarousel from '@/components/ComicCarousel.vue'
+import ReaderLayout from '@/Layouts/ReaderLayout.vue'
+
+
+const props = defineProps({
+  comic: {
+    type: Object,
+    default: null
+  },
+
+  errors: Object,
+})
+</script>
