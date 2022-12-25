c25a8d0baaa8cbcd53288791a2f14aeef4b38850
leaveitblank32 <leaveitblank32@gmail.com>
2022-12-25 15:59:42 +0530

    minor changes

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -1,40 +1,15 @@
 #!/bin/bash
-echo "perms"
- 
 cd /workspace
 echo "Starting"
-
-echo ls -la
+echo $ls -la
 sudo cp .env.example .env
-
 echo "Composer Started"
 sudo composer install 
 echo "Composer Finished"
-echo "Key Generated"
-php artisan key:generate
-echo "Key Generated:finsihed"
 echo "NPM Started"  
 sudo npm install
 echo "NPM Finished"
 npm run production
 echo "NPM Build Finished"
 echo "Storage Link Created"
-php artisan storage:link
-echo "Storage Link Created:Finished"
-echo "OPTIMIZE Started"
-php artisan optimize
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-echo "OPTIMIZE Finished"
-echo "Server Ready"
-#echo "Migration Started" 
-#sudo php artisan migrate --seed  
-#yes
-#"yes"
-#echo "yes"
-echo "migrate db from docker" 
-echo "Queue start from docker "
-#php artisan horizon
-#command php artisan horizon -d
+ 
