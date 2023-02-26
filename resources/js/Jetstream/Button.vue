e25abb0abc0df2dbee46360dd79140cb4e8d2fde
Leave-it-blank <70465598+Leave-it-blank@users.noreply.github.com>
2023-02-26 16:23:15 +0530

    updated button color

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-purple-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 active:bg-purple-400 focus:outline-none focus:border-purple-900 focus:ring focus:ring-purple-300 disabled:opacity-25 transition">
         <slot />
     </button>
 </template>
