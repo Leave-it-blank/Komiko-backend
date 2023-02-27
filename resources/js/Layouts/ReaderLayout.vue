ff125e358d929c5ded7cdddc7dc5af5a76e5f13e
Leave-it-blank <70465598+Leave-it-blank@users.noreply.github.com>
2023-02-28 04:10:23 +0530

    test_1 ads

--- resources/js/Layouts/ReaderLayout.vue
+++ resources/js/Layouts/ReaderLayout.vue
@@ -8,6 +8,13 @@ mainStore.setStyle("reader");
 
 const ads_global_1 = usePage().props.value.ads_global.below_rec;
 const ads_global_2 = usePage().props.value.ads_global.above_rec;
+    document.addEventListener("DOMContentLoaded", function () {
+      const b = document.querySelector("#ads_global_nav_1");
+      b.insertAdjacentHTML("afterbegin", ads_global_1);
+        const a = document.querySelector("#ads_global_nav_2");
+      a.insertAdjacentHTML("afterbegin", ads_global_2);
+    });
+
 </script>
 
 <template>
@@ -15,11 +22,11 @@ const ads_global_2 = usePage().props.value.ads_global.above_rec;
   <main class="relative">
     <div class="w-full h-full min-h-screen">
       <ReaderNavBar />
-      <div v-html="ads_global_1" id="ads_global_nav"></div>
+      <s   id="ads_global_nav_1"></div>
 
       <slot />
     </div>
-    <div v-html="ads_global_2" id="ads_global_nav"></div>
+    <div   id="ads_global_nav_2"></div>
     <ReaderFooter />
   </main>
 </template>
